d1c60cd143563d66ab8ebc6cce52dd743ca20db6
Kalyan Manna <kalyanmanna439@gmail.com>
2025-08-17 00:03:31 +0530

    chore: update server.js config and middleware setup

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ const app = require("./app");
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.send("Server is running well!")
+    res.send("Server is running well!🚀")
 })
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
