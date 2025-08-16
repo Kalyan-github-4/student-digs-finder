2e752b240d8f7bc230bb6a82ba72eca6ae742c68
Kalyan Manna <kalyanmanna439@gmail.com>
2025-08-17 00:02:45 +0530

    feat: update App.tsx with new routes and providers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AccommodationProvider } from "@/contexts/AccommodationProvider";
+import { AccommodationProvider  } from "@/contexts/AccommodationContext";
 import Index from "./pages/Index";
 import BrowseAccommodation from "./pages/BrowseAccommodation";
 import ListYourProperty from "./pages/ListYourProperty";
@@ -29,7 +29,7 @@ const App = () => {
   return (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <AccommodationProvider>
+      <AccommodationProvider >
         <Toaster />
         <Sonner />
         <BrowserRouter>
