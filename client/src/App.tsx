68b3e1e8c602791649e54353cd42bee8ef721a7a
Kalyan Manna <kalyanmanna439@gmail.com>
2025-08-16 10:58:56 +0530

    Update App.tsx to integrate new components and context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AccommodationProvider } from "@/contexts/AccommodationContext";
+import { AccommodationProvider } from "@/contexts/AccommodationProvider";
 import Index from "./pages/Index";
 import BrowseAccommodation from "./pages/BrowseAccommodation";
 import ListYourProperty from "./pages/ListYourProperty";
