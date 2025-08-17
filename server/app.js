92dce55eb8f5bcef1d49dcea87131372798b48d7
Kalyan Manna <kalyanmanna439@gmail.com>
2025-08-17 20:02:44 +0530

    Update server configuration and middleware setup

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const bodyparser = require('body-parser');
 const propertyRoutes = require('./routes/propertyRoutes');
 const cors = require('cors');
 const app = express();
+const path = require("path");
 
 app.use(cors({
   origin: 'http://localhost:8080', 
@@ -11,12 +12,20 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+
 //Middleware
 app.use(bodyparser.json({limit: '20mb'})); // Increased limit for large property data
 app.use(bodyparser.urlencoded({ limit: '20mb',extended: true}));
 
 //Routes
 app.use("/api/properties", propertyRoutes)
+
+
+// app.use(express.static(path.join(__dirname, "client/build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client/build", "index.html"));
+// });
 
 //Error
 app.use((err, req, res, next) => {
