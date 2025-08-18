8bb34dfb063ac070890b815aa0d0c9985daab5ce
Kalyan Manna <kalyanmanna439@gmail.com>
2025-08-18 17:43:43 +0530

    Update AccommodationContext.tsx

--- client/src/contexts/AccommodationContext.tsx
+++ client/src/contexts/AccommodationContext.tsx
@@ -48,7 +48,7 @@ export const AccommodationProvider = ({ children }: { children: ReactNode }) => 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/properties"); // <-- your backend route
+        const response = await fetch("http://localhost:5000/api/properties/api/properties"); // <-- your backend route
         const data: Accommodation[] = await response.json();
         setAccommodations(data);
       } catch (error) {
